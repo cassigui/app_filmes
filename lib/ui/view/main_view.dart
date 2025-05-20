45f65ceb76fd448e502d653324868eb7b9303e75
Igor <igoreduardo20101@gmail.com>
2025-05-20 19:02:50 -0300

    corrige account view dentro da main

--- lib/ui/view/main_view.dart
+++ lib/ui/view/main_view.dart
@@ -24,7 +24,7 @@ class MainView extends StatelessWidget {
           children: [
             MovieListPage(),
             FavoriteView(),
-            FavoriteView(),
+            AccountView(),
           ],
         ),
       ),
